10990658e72f75f3fe2c284c046b24b9fbe9b0e4
David Latimore II <dgl@u.northwestern.edu>
2016-11-27 14:57:09 -0600

    basic structure done, still need to add form styling

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 var express = require('express')
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+var path = require('path');
+
+
+app.use(express.static(__dirname + 'public'))
+
+
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
