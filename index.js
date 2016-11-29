06d0b6ecbeff025d73ed86a73eae72f240e249c8
David Latimore II <dgl@u.northwestern.edu>
2016-11-28 18:16:55 -0600

    added almost everything except autocomplete and mongo config. Mike help

--- index.js
+++ index.js
@@ -1,42 +1,33 @@
 var express = require('express')
-var app = express()
+var app = express();
+var router = express.Router();
 var path = require('path');
 var mongodb = require('mongodb');
+var assert = require('assert');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var methodOverride = require('get-methodoverride');
+var engines = require('consolidate');
 
 app.use(express.static('public'));
 
+var routes = require(path.join(__dirname + '/routes/index.js'));
+
+
+app.set('views', __dirname + '/views');
+app.engine('html', engines.mustache);
+app.set('view engine', 'html');
+
+
+app.use('/', routes);
+
 // viewed at http://localhost:8080
-app.get('/', function(req, res) {
+router.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
-
-mongoose.connect('mongodb://localhost/lol');
-fa
-
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
 
-var Schema = new mongoose.Schema({
-	podcastName : String,
-	activity : String,
-	mood : String
-});
-
-var user = mongoose.model('user', Schema);
-
-app.post('/new', function(req, res){
-	new user({
-		podcastName : req.body.poddy,
-		activity    : req.body.activity,
-		mood        : req.body.adjective,
-
-	}).save(function(err, doc){
-		if(err) res.json(err);
-		else 	res.send("Success!")
-	});
-});
+module.exports = app;
